44d0943359a9027b546750b6a6b1e54fb23dbebb
coa989 <a.marjanovic989@gmail.com>
2021-06-16 18:35:33 +0200

    created routes and views for admin actions

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,12 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function index()
     {
-        return view('dashboard');
+        return view('admin.dashboard');
+    }
+
+    public function allPosts()
+    {
+        $posts = Post::all();
+        return view('admin.posts.index', ['posts' => $posts]);
+    }
+
+    public function approvedPosts()
+    {
+        $posts = Post::where('approved', true)->get();
+        return view('admin.posts.approved', ['posts' => $posts]);
+    }
+
+    public function waitingPosts()
+    {
+        $posts = Post::where('approved', false)->get();
+        return view('admin.posts.waiting', ['posts' => $posts]);
     }
 }
