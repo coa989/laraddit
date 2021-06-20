16eb00ef2335974305b56a6f76255f2288736758
coa989 <a.marjanovic989@gmail.com>
2021-06-20 20:23:58 +0200

    user total points, p per definition and per post

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,10 +12,59 @@ class UserController extends Controller
     public function show(User $user)
     {
         $userWith = User::where('id', $user->id)
-            ->with('posts', 'comments', 'likes')
+            ->with('posts', 'definitions', 'comments', 'likes')
             ->first();
 
-        return view('users.show', ['user' => $userWith]);
+        $posts = Post::where('approved', true)
+            ->where('user_id', $user->id)
+            ->with('user', 'tags')
+            ->latest()
+            ->paginate(15);
+
+        $definitions = Definition::where('approved', true)
+            ->where('user_id', $user->id)
+            ->with('user', 'tags')
+            ->latest()
+            ->paginate(15);
+
+        if ($definitions->first()) {
+            foreach ($definitions as $definition) {
+                $points[] = $definition
+                        ->likes()
+                        ->where('is_dislike', 0)
+                        ->get()->count() - $definition
+                        ->likes()
+                        ->where('is_dislike', 1)
+                        ->get()
+                        ->count();
+                $definitionPoints = array_sum($points);
+            }
+        } else {
+            $definitionPoints = 0;
+
+        }
+;
+        if ($posts->first()) {
+            foreach ($posts as $post) {
+                $points[] = $post
+                        ->likes()
+                        ->where('is_dislike', 0)
+                        ->get()->count() - $post
+                        ->likes()
+                        ->where('is_dislike', 1)
+                        ->get()
+                        ->count();
+                $postPoints = array_sum($points);
+            }
+        } else {
+            $postPoints = 0;
+        }
+
+        return view('users.show', [
+            'user' => $userWith,
+            'postPoints' => $postPoints,
+            'definitionPoints' => $definitionPoints
+        ]);
     }
 
     public function posts(User $user)
